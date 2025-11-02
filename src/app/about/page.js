55deaa9d381e7d895ebbaa698189574a04f700de
Pushkar Gupta <pushkargupta606@gmail.com>
2025-11-03 00:52:59 +0530

    Change CTA link from '/contact' to '/insights'

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -107,7 +107,7 @@ export default function About() {
         {/* CTA */}
         <div className="text-center mt-16">
           <a
-            href="/contact"
+            href="/insights"
             className="inline-block bg-blue-600 text-white px-8 py-4 rounded-lg text-lg font-semibold hover:bg-blue-700 transition-colors"
           >
             Get Started Today
